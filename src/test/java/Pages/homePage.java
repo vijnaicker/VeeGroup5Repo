d0e6cf530066b6e44b78af70808ef39b121d42c4
Vijhan <vijnaicker@gmail.com>
2026-08-22 06:22:34 -0700

    added code for login button

--- src/test/java/Pages/homePage.java
+++ src/test/java/Pages/homePage.java
@@ -1,4 +1,20 @@
 package Pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class homePage {
+    WebDriver driver;
+
+    @FindBy(xpath = "//span[contains(text(),'Login')]")
+    WebElement loginButton;
+
+    public homePage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void clickLoginButton(){
+        loginButton.click();
+    }
 }
